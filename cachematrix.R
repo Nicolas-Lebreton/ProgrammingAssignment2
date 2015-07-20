6b65ce6aba93908543e407ba3e12895fc6788031
Nicolas LEBRETON <nicolas.lebreton@horus-oi.com>
2015-07-20 13:57:00 +0400

    Commit of "cachematrix.R" for Programming Assignment 2 on Coursera R Programming Course

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function cache a matrix in another environment in order to retrieve it instead of recompute it 
 
+makeCacheMatrix <- function(x = matrix()) {
+        mat <- NULL
+        set <- function(y) {
+            x <<- y
+            mat <<- NULL
+        }
+        get <- function() x
+        setMatrix <- function(myMatrix) mat <<- myMatrix
+        getMatrix <- function() mat
+        list(set = set, get = get,
+                setMatrix = setMatrix,
+                getMatrix = getMatrix)
 }
+
+
 
 
 ## Write a short comment describing this function
 
+## Make a calculation or retrieve it from cache
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mat <- x$getMatrix()
+        if(!is.null(mat)) {
+            message("getting cached data")
+            return(mat)
+        }
+        
+        data <- x$get()
+        
+        mat <- solve(data, ...)
+        x$setMatrix(mat)
+        
+        return(mat)
 }
